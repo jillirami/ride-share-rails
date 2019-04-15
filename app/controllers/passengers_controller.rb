ea7a3242b9ed9e16cc1961525463d10f11ca8ce1
Jillianne Ramirez <jillianne.ramirez@gmail.com>
2019-04-15 15:48:38 -0700

    index, show and new actions added to Passenger/Trip controllers

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -1,7 +1,17 @@
 class PassengersController < ApplicationController
   def index
+    @passengers = Passenger.all.order(:name)
   end
 
   def show
+    passenger_id = params[:id]
+    @passenger = Passenger.find_by(id: passenger_id)
+    if @passenger.nil?
+      head :not_found
+    end
+  end
+
+  def new
+    @passenger= Passenger.new(name: "Default Passenger")
   end
 end
--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,7 +1,19 @@
 # frozen_string_literal: true
 
 class TripsController < ApplicationController
-  def index; end
+  def index 
+    @trips = Trip.all.order(:id)
+  end
 
-  def show; end
+  def show 
+    trip_id = params[:id]
+    @trip = Trip.find_by(id: trip_id)
+    if @trip.nil?
+      head :not_found
+    end
+  end
+
+  def new
+    @trip = Trip.new()
+  end
 end
