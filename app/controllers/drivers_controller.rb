0ba2abbff957535dcb0f16cd986e3ffb21824608
Ariana Bray <aribray@gmail.com>
2019-04-15 15:51:01 -0700

    moved index and show actions from trips to driver

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,7 +1,17 @@
+# frozen_string_literal: true
+
 class DriversController < ApplicationController
   def index
+    @drivers = Driver.all.order(:id)
   end
 
   def show
+    driver_id = params[:id]
+    @driver = Driver.find_by(id: driver_id)
+    head :not_found if @driver.nil?
+  end
+
+  def new
+    @driver = Driver.new
   end
 end
--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,19 +1,5 @@
 # frozen_string_literal: true
 
 class TripsController < ApplicationController
-  def index 
-    @trips = Trip.all.order(:id)
-  end
-
-  def show 
-    trip_id = params[:id]
-    @trip = Trip.find_by(id: trip_id)
-    if @trip.nil?
-      head :not_found
-    end
-  end
-
-  def new
-    @trip = Trip.new()
-  end
+  
 end
