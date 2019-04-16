69c82997ff33a67fcbc88dbda186562c91a0f864
Ariana Bray <aribray@gmail.com>
2019-04-16 15:18:16 -0700

    added create new trip functionailty and forms

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -10,4 +10,27 @@ class TripsController < ApplicationController
     @trip = Trip.find_by(id: trip_id)
     redirect_to drivers_path if @trip.nil?
   end
+
+  def new
+    @trip = Trip.new
+  end
+
+  def create
+    trip = Trip.new(trip_params)
+
+    is_successful = driver.save
+
+    if is_successful
+      redirect_to trip_path(trip.id)
+    else
+      @trip = trip
+      render :new
+    end
+  end
+
+  private
+
+  def trip_params
+    params.require(:trip).permit(:date, :rating, :cost, :driver_id, :passenger_id)
+  end
 end
