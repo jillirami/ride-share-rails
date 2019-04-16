5c6c2250921dab6fa3f52fe89598ff5164fa511e
Jillianne Ramirez <jillianne.ramirez@gmail.com>
2019-04-16 11:16:41 -0700

    show and avg_rating functions added to driver

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,3 +1,15 @@
 class Driver < ApplicationRecord
   has_many :trips
+
+  def avg_rating
+    ratings = []
+    self.trips.each do |t|
+      ratings << t.rating
+    end
+    avg_rating = ratings.inject{ |sum, el| sum + el }.to_f / ratings.length 
+    return avg_rating.round(2)
+  end
+
+  validates :name, presence: true
+  validates :vin, presence: true, uniqueness: true
 end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,7 @@
 class Trip < ApplicationRecord
   belongs_to :passenger
   belongs_to :driver
+
+  validates :driver_id, presence: true
+  validates :passenger_id, presence: true
 end
