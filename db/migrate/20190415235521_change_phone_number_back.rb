285c4a8a12d5b84289ec1190fbd47951bcdc2e43
Jillianne Ramirez <jillianne.ramirez@gmail.com>
2019-04-16 16:54:01 -0700

    status column added for drivers

--- db/migrate/20190415235521_change_phone_number_back.rb
+++
@@ -1,5 +0,0 @@
-class ChangePhoneNumberBack < ActiveRecord::Migration[5.2]
-  def change
-    rename_column :passengers, :phone_number, :phone_num
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_15_235521) do
+ActiveRecord::Schema.define(version: 2019_04_16_235139) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2019_04_15_235521) do
     t.boolean "active"
     t.string "car_make"
     t.string "car_model"
+    t.boolean "status"
   end
 
   create_table "passengers", force: :cascade do |t|
